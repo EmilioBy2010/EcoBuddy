3eb49ecb496413f085fc56ac3b10860faa30a4f6
EmilioBy2010 <emiliovillagran2010@gmail.com>
2025-01-23 13:13:46 -0600

    Update EcoBuddy.py

--- EcoBuddy.py
+++ EcoBuddy.py
@@ -6,7 +6,7 @@ from juego import juegos
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='$', intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
